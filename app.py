968305d5b6bd96f4bf3d9eac830c795f8cc4c2bf
Rais Iqbal <bahshat@gmail.com>
2024-02-12 19:19:04 +0800

    Database added

--- app.py
+++ app.py
@@ -2,34 +2,27 @@ import json
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
 
+from db_operations import DB
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-# Example API route (replace with your specific logic)
 @app.route('/')
-@cross_origin()
-def get_home():
-    return "<h1>This is a Backend service for a Knowledeg Project.</h1>"
-
-# Example API route (replace with your specific logic)
-@app.route('/api/questions')
+@app.route('/api/questions/')
 @cross_origin()
 def get_questions():
-    # Open the JSON file in read mode
-    with open('question.json', 'r') as f:
-        try:
-            # Load the JSON data
-            data = json.load(f)
-        except FileNotFoundError:
-            # Handle file not found error (return suitable error response)
-            return jsonify({'error': 'JSON file not found'}), 404
-        except json.JSONDecodeError:
-            # Handle invalid JSON data error (return suitable error response)
-            return jsonify({'error': 'Invalid JSON data'}), 400
-
-    # Return the JSON data as a JSON response
+    print("[INVOKED] route::get_questions")
+    data = DB().getQuestions()
     return jsonify(data)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+
+@app.route('/api/answer/<int:questionId>')
+@cross_origin()
+def get_answers_for_question(questionId):
+    print("[INVOKED] route::get_get_answers_for_question")
+    data = DB().getAnswers(questionId)
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
